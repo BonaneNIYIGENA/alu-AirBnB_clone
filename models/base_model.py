fedc51bbc46346fd2745b37db12925639731c2c0
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-02-05 14:12:39 +0200

    base modelling

--- models/base_model.py
+++ models/base_model.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Module for BaseModel Class"""
 import uuid
 from datetime import datetime
-import models
 
 
 class BaseModel:
@@ -20,6 +18,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            import models
             models.storage.new(self)
 
     def __str__(self):
