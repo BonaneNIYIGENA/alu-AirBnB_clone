ddd854c6c6d77acf64b52d9cefc696abd0105760
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-02-05 14:25:08 +0200

    modals

--- models/base_model.py
+++ models/base_model.py
@@ -1,41 +1,42 @@
 #!/usr/bin/python3
+"""BaseModel for all other classes"""
 import uuid
 from datetime import datetime
+import models
 
 
 class BaseModel:
-    """Defines all common attributes/methods for other classes"""
-
     def __init__(self, *args, **kwargs):
-        """Initializes the instance"""
+        """constructor method for the class"""
         if kwargs:
             for key, value in kwargs.items():
+                if key == "__class__":
+                    continue
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
+                setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
-            import models
             models.storage.new(self)
 
     def __str__(self):
-        """Returns string representation of the instance"""
-        cls_name = self.__class__.__name__
-        return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
+        """returns the string representation"""
+        class_name = self.__class__.__name__
+        return f"[{class_name}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        """Updates updated_at with current datetime"""
+        """updates updated_at attribute with current datetime"""
         self.updated_at = datetime.now()
-        import models
         models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of __dict__"""
-        res_dict = self.__dict__.copy()
-        res_dict['__class__'] = self.__class__.__name__
-        res_dict['created_at'] = self.created_at.isoformat()
-        res_dict['updated_at'] = self.updated_at.isoformat()
-        return res_dict
+        """returns a dictionary containing all the keys and values
+        of the instance's __dict__."""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = self.__class__.__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
+    
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,51 +1,55 @@
 #!/usr/bin/python3
-"""Module for FileStorage class"""
+
 import json
-from models.base_model import BaseModel
 
 
-class FileStorage:
-    """Serializes instances to JSON and deserializes JSON to instances"""
-    __file_path = "file.json"
+class FileStorage():
+    """
+    class that serializes instances to a JSON file and deserializes JSON file to instances
+    """
+    __file_path = 'file.json'
     __objects = {}
 
     def all(self):
         """Returns the dictionary __objects"""
-        return FileStorage.__objects
+        return self.__objects
 
     def new(self, obj):
-        """Sets in __objects the obj with key <obj class name>.id"""
+        """Sets obj in __objects with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        self.__objects[key] = obj
 
     def save(self):
+        from models.base_model import BaseModel
         """Serializes __objects to the JSON file"""
-        new_dict = {}
-        for key, value in FileStorage.__objects.items():
-            new_dict[key] = value.to_dict()
-        with open(FileStorage.__file_path, "w") as f:
-            json.dump(new_dict, f)
+        data = {}
+        for key, obj in FileStorage.__objects.items():
+            data[key] = obj.to_dict()
+        with open(FileStorage.__file_path, 'w') as file:
+            json.dump(data, file)
 
     def reload(self):
-        """Deserializes the JSON file to __objects"""
         from models.base_model import BaseModel
         from models.user import User
-        from models.place import Place
         from models.state import State
         from models.city import City
         from models.amenity import Amenity
         from models.review import Review
+        from models.place import Place
 
-        classes = {
+        class_name = {
             "BaseModel": BaseModel, "User": User, "State": State,
-            "City": City, "Amenity": Amenity, "Place": Place,
-            "Review": Review
-        }
+            "City": City, "Amenity": Amenity,
+            "Place": Place, "Review": Review
+            }
+
         try:
-            with open(FileStorage.__file_path, "r") as f:
-                obj_dict = json.load(f)
-            for key, val in obj_dict.items():
-                cls_name = val["__class__"]
-                FileStorage.__objects[key] = eval(cls_name)(**val)
+            with open(self.__file_path, 'r') as file:
+                data = json.load(file)
+                for key, obj_dict in data.items():
+                    class_name, obj_id = key.split('.')
+                    obj = eval(class_name)(**obj_dict)
+                    self.__objects[key] = obj
         except FileNotFoundError:
             pass
+        
