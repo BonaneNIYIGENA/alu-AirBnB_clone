389723e0dae682f338968de49c20b7f3c01b5dea
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-02-05 14:09:24 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -30,6 +30,7 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current datetime"""
         self.updated_at = datetime.now()
+        import models
         models.storage.save()
 
     def to_dict(self):
