8059b30a6149a0b6e0efd56e72d84f12f5147e40
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-02-05 12:53:10 +0200

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,37 +1,41 @@
 #!/usr/bin/python3
-
-"""Defines the BaseModel class."""
+"""Module for BaseModel Class"""
 import uuid
 from datetime import datetime
 import models
 
+
 class BaseModel:
-    """Represents the BaseModel of the AirBnB clone project."""
+    """Defines all common attributes/methods for other classes"""
+
     def __init__(self, *args, **kwargs):
-        """Initializes a new BaseModel instance."""
+        """Initializes the instance"""
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
                     value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                elif key != "__class__":
+                if key != "__class__":
                     setattr(self, key, value)
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
- 
-        models.storage.new(self)
+            models.storage.new(self)
 
     def __str__(self):
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
-    
+        """Returns string representation of the instance"""
+        cls_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(cls_name, self.id, self.__dict__)
+
     def save(self):
+        """Updates updated_at with current datetime"""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
-        """Returns a dictionary containing all keys/values of the instance."""
-        my_dict = self.__dict__.copy()
-        my_dict["__class__"] = self.__class__.__name__
-        my_dict["created_at"] = self.created_at.isoformat()
-        my_dict["updated_at"] = self.updated_at.isoformat()
-        return my_dict
+        """Returns a dictionary containing all keys/values of __dict__"""
+        res_dict = self.__dict__.copy()
+        res_dict['__class__'] = self.__class__.__name__
+        res_dict['created_at'] = self.created_at.isoformat()
+        res_dict['updated_at'] = self.updated_at.isoformat()
+        return res_dict
