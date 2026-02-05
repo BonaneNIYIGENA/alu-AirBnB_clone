55efed52821501b9b95616bef5d2bf47f501d897
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-02-05 13:35:56 +0200

    classes

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,8 +33,8 @@ class FileStorage:
         from models.place import Place
         from models.state import State
         from models.city import City
-        from models.amenity import amenity
-        from models.review import review
+        from models.amenity import Amenity
+        from models.review import Review
 
         classes = {
             "BaseModel": BaseModel, "User": User, "State": State,
