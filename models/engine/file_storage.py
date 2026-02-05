1cbc29e9c378c7ea748ecb3983d7aef24bcb1fd2
BonaneNIYIGENA <b.niyigena@alustudent.com>
2026-02-05 12:55:24 +0200

    file storage modal

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
-
-"""Defines the BaseModel class."""
+"""Module for FileStorage class"""
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
+    """Serializes instances to JSON and deserializes JSON to instances"""
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        """Returns the dictionary __objects."""
+        """Returns the dictionary __objects"""
         return FileStorage.__objects
-    
+
     def new(self, obj):
-        """Sets in __objects the obj with key <obj_class_name>.id"""
+        """Sets in __objects the obj with key <obj class name>.id"""
         key = "{}.{}".format(obj.__class__.__name__, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """Serializes __objects to the JSON file"""
         new_dict = {}
         for key, value in FileStorage.__objects.items():
             new_dict[key] = value.to_dict()
@@ -25,11 +27,12 @@ class FileStorage:
             json.dump(new_dict, f)
 
     def reload(self):
+        """Deserializes the JSON file to __objects"""
         try:
             with open(FileStorage.__file_path, "r") as f:
                 obj_dict = json.load(f)
-            for key, value in obj_dict.items():
-                class_name = value["__class__"]
-                FileStorage.__objects[key] = eval(class_name)(**value)
+            for key, val in obj_dict.items():
+                cls_name = val["__class__"]
+                FileStorage.__objects[key] = eval(cls_name)(**val)
         except FileNotFoundError:
             pass
